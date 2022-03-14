ca1ddd906299c16c67e2a96d655287d4f5b1efeb
mohamedtamer0 <mt4452242@gmail.com>
2022-03-15 00:09:27 +0200

    update

--- JavaAdvanced/src/com/company/S_append.java
+++ JavaAdvanced/src/com/company/S_append.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.StringTokenizer;
+
 public class S_append {
     public static void main(String[] args) {
         StringBuffer name = new StringBuffer("Mohamed");
@@ -7,17 +9,24 @@ public class S_append {
 
         System.out.println(name);
 
-        name.insert(0,"MR: ");
+        name.insert(0, "MR: ");
         System.out.println(name);
         System.out.println(name.capacity());
 
         StringBuilder name2 = new StringBuilder("Mohamed");
         StringBuilder name3 = new StringBuilder("Mohamed");
-        name2.insert(7," Tamer");
+        name2.insert(7, " Tamer");
         System.out.println(name2);
 
-        name3.replace(0,4,"Ah");
+        name3.replace(0, 4, "Ah");
         System.out.println(name3);
+
+        StringTokenizer str = new StringTokenizer("My Name is Mohamed ,Tamer");
+        while (str.hasMoreTokens()) {
+            System.out.println(str.nextToken());
+        }
+        StringTokenizer str2 = new StringTokenizer("My Name is Mohamed ,Tamer");
+        System.out.println(str2.nextToken(","));
 
     }
 }
