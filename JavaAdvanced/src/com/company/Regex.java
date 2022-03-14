d5185dd7a08b87a2aff1944aed10a435fae6fb65
mohamedtamer0 <mt4452242@gmail.com>
2022-03-15 00:57:26 +0200

    update

--- JavaAdvanced/src/com/company/Regex.java
+++ JavaAdvanced/src/com/company/Regex.java
@@ -22,15 +22,43 @@ public class Regex {
         System.out.println("==================");
 
 
-        boolean m = Pattern.matches("[abc]","f");
+        boolean m = Pattern.matches("[abc]", "f");
         System.out.println(m);
         System.out.println("==================");
-        boolean m2 = Pattern.matches("[a-z]","f");
+        boolean m2 = Pattern.matches("[a-z]", "f");
         System.out.println(m2);
         System.out.println("==================");
-        boolean m3 = Pattern.matches("[A-Z]","f");
+        boolean m3 = Pattern.matches("[A-Z]", "f");
         System.out.println(m3);
 
+        System.out.println("==================");
+
+        boolean v = Pattern.matches("[A-F&&[def]]", "F");
+        boolean z = Pattern.matches("[a-z&&[^fd]]", "d");
+        System.out.println(v);
+        System.out.println(z);
+
+
+
+        System.out.println("==================");
+
+
+
+        boolean p = Pattern.matches("[muh]?","m");
+        boolean p1 = Pattern.matches("[muh]?","mmm");
+        boolean p2 = Pattern.matches("[muh]?","muh");
+        System.out.println(p);
+        System.out.println(p1);
+        System.out.println(p2);
+
+        System.out.println("==================");
+
+        boolean p3 = Pattern.matches("[muh]+","m");
+        boolean p4 = Pattern.matches("[muh]+","mmm");
+        boolean p5 = Pattern.matches("[muh]+?","muhm");
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
 
     }
 }
