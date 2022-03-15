addf596081a2cfb0001d2468dea39f19f86962fe
mohamedtamer0 <mt4452242@gmail.com>
2022-03-16 00:44:51 +0200

    update

--- JavaAdvanced/src/com/company/MultiThreading.java
+++ JavaAdvanced/src/com/company/MultiThreading.java
@@ -4,6 +4,14 @@ public class MultiThreading extends Thread {
     @Override
     public void run() {
         System.out.println("Mohamed Tamer");
+        for (int i = 5; i < 15; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
     }
 
     public static void main(String[] args) {
@@ -12,6 +20,8 @@ public class MultiThreading extends Thread {
         Main2 obj1 = new Main2();
         Thread obj2 = new Thread(obj1);
         obj2.start();
+
+        obj.start();
     }
 }
 
