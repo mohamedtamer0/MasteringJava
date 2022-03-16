3e5862d0ff97d626f1a33dae430141839e246219
mohamedtamer0 <mt4452242@gmail.com>
2022-03-16 20:18:24 +0200

    update

--- JavaAdvanced/src/com/company/Main.java
+++ JavaAdvanced/src/com/company/Main.java
@@ -1,11 +1,21 @@
 package com.company;
 
 
+import com.sun.net.httpserver.HttpContext;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-
+        URL url = new URL("https://github.com/mohamedtamer0");
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(httpURLConnection.getHeaderFieldKey(i) + "=" + httpURLConnection.getHeaderField(i));
+        }
+        httpURLConnection.disconnect();
     }
 }
